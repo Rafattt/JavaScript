793fa8d6da6be03d86aeb823bef3f438dc8c98bd
Rafal Kmiecik <Rafattt@users.noreply.github.com>
2017-09-06 14:14:39 -0500

    Add files via upload

--- fcc/Calculator/calculator.js
+++ fcc/Calculator/calculator.js
@@ -3,12 +3,8 @@ $(document).ready(function(){
 	var lastActiveKey; //variable for storing last clicked button value
 	var typedNumbers = "0"; //adding typed keys to this variable as a string
 	$("#main-text").html(typedNumbers);
-	
-	var numbersArr = [];
-	var i = 0;
-	var tempResult;
+	var subtext;  //storing numbers for displaing bootom line on the screen
 	var result;
-	var resultOld = 0;
 	
 	function pushTheButton(){ //this function checking name of div id, setting it as lastActiveKey and adding it to typedNumbers string
 		lastActiveKey = event.target.id;
@@ -18,9 +14,11 @@ $(document).ready(function(){
 	function checkIfFirstZero(){    //this function checking if zero is a first character and if it is removing it from string
 		if(typedNumbers.indexOf("0") === 0){
 			typedNumbers = typedNumbers.substr(1);
-			$("#main-text").html(typedNumbers);
+			$("#main-text").html(lastActiveKey);
+			$("#sub-text").html(typedNumbers);
 		} else {
-			$("#main-text").html(typedNumbers);
+			$("#main-text").html(lastActiveKey);
+			$("#sub-text").html(typedNumbers);
 		}
 	}
 	
@@ -43,7 +41,7 @@ $(document).ready(function(){
 			pushTheZero();
 		});
 		$("#5").click(function(){
-			pushTheZero();tiveKey;
+			pushTheZero();
 		});
 		$("#6").click(function(){
 			pushTheZero();
@@ -64,10 +62,53 @@ $(document).ready(function(){
 			$("#main-text").html(typedNumbers);
 			}
 		});
+		//can't automaticly read value of these values so using checkIfFirstZero() only
 		$("#button-dot").click(function(){
 			lastActiveKey = ".";
 			typedNumbers = typedNumbers + lastActiveKey;
 			checkIfFirstZero();
+		});
+		$("#plus").click(function(){
+			lastActiveKey = "+";
+			typedNumbers = typedNumbers + lastActiveKey;
+			checkIfFirstZero();
+		});
+		$("#minus").click(function(){
+			lastActiveKey = "-";
+			typedNumbers = typedNumbers + lastActiveKey;
+			checkIfFirstZero();
+		});
+		$("#divide").click(function(){
+			lastActiveKey = "/";
+			typedNumbers = "(" + typedNumbers + ")" + lastActiveKey;
+			checkIfFirstZero();
+		});
+		$("#multiply").click(function(){
+			lastActiveKey = "*";
+			typedNumbers = "(" + typedNumbers + ")" + lastActiveKey;
+			checkIfFirstZero();
+		});
+		$("#equal-button").click(function(){
+			typedNumbers = typedNumbers.toString(); 
+			typedNumbers = typedNumbers.replace(/,/g, "");
+			result = eval(typedNumbers);
+			typedNumbers = result;
+			$("#main-text").html(result);
+		});
+		$("#ac").click(function(){
+			lastActiveKey = ''; 
+			typedNumbers = '0'; 
+			subtext ='0';  //storing numbers for displaing bootom line on the screen
+			$("#main-text").html(typedNumbers);
+			$("#sub-text").html(subtext);
+		});
+		$("#ce").click(function(){
+			if(typedNumbers.length > 1){
+				lastActiveKey = typedNumbers.charAt(typedNumbers.length-2); 
+				typedNumbers = typedNumbers.slice(0, -1);
+				$("#main-text").html(lastActiveKey);
+				$("#sub-text").html(typedNumbers);
+			}
 		});
 	})();
 	
