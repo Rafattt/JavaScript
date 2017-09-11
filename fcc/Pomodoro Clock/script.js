8c31378298f83ad8289e32c23a8af85fc5caa6ba
Rafal Kmiecik <Rafattt@users.noreply.github.com>
2017-09-11 12:45:27 -0500

    Update script.js
    updated session timer and added pre-alpha break timer

--- fcc/Pomodoro Clock/script.js
+++ fcc/Pomodoro Clock/script.js
@@ -1,18 +1,22 @@
 $(document).ready(function(){
 	var currentTime = new Date();
 	var breakLength = 5;
+	var breakLeft = 5;
 	var sessionLength = 25;
 	var timeLeft = 25;
 	var seconds = 15;
+	var controlNumber = 0;
 	
 	
 	$("#break-length").html(breakLength);
 	$("#minus-break").click(function(){
 		breakLength--;
+		breakLeft = breakLength;
 		$("#break-length").html(breakLength);
 	});
 	$("#plus-break").click(function(){
 		breakLength++;
+		breakLeft = breakLength;
 		$("#break-length").html(breakLength);
 	});
 	
@@ -30,37 +34,70 @@ $(document).ready(function(){
 		$("#session-clock").html(timeLeft);
 		$("#session-length").html(sessionLength);
 	});
+
 	
 	
 	
+
 	
-	$("#pomodoro-box").click(function(){
+	
+	$("#pomodoro-box").click(function sestest(){
 		timeLeft--;
+		seconds--;
+		console.log(timeLeft);
 		function timer(){
 		$("#session-clock").html(timeLeft+":"+seconds);
-		console.log(seconds);
-		if(seconds<10 && seconds>0 && timeLeft!= 0){
-			seconds = "0"+seconds;
-			$("#session-clock").html(timeLeft+":"+seconds);
-		} else if(seconds === 0){
+		if(seconds<10 && seconds>0){
+			$("#session-clock").html(timeLeft+":0"+seconds);
+			console.log("one");
+			seconds--;
+		} else if(seconds === 0 && timeLeft>0){
 			$("#session-clock").html(timeLeft+":"+"00");
-		} else if(seconds<0){
+			console.log("two");
+			seconds--;
+		} else if(seconds<0 && timeLeft>0){
 			seconds = 15;
 			timeLeft--;
 			$("#session-clock").html(timeLeft+":"+seconds);
+			console.log("three");
+			seconds--;
 		} else if(seconds==0 && timeLeft==0){
-			return;
+			console.log("here");
+			breakLeft = breakLength;
+			clearInterval(refreshIntervalId);
+			breakTime()
+			controlNumber = 1;
 		} else{
 			seconds--;
 		}
-		
 	};
 
-		setInterval(function(){
+		var refreshIntervalId = setInterval(function(){
 			timer();
 		}, 1000);
 	
 	});
+	
+	
+	
+		function breakTime(){
+		seconds = 15;
+		function timer2(){
+			$("#session-text").html("Break");
+				console.log("good");
+				$("#session-clock").html(breakLeft+":"+seconds);
+				
+		
+			seconds--;
+		};
+				
+		
+				setInterval(function(){
+			timer2();
+		}, 1000);
+
+	};
+	
 	
 	
 	
