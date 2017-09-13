69cbd5246574982c89f914e04481cb5dbde4d093
Rafal Kmiecik <Rafattt@users.noreply.github.com>
2017-09-13 15:11:29 -0500

    Add files via upload

--- fcc/Pomodoro Clock/scriptSecondVersion.js
+++ fcc/Pomodoro Clock/scriptSecondVersion.js
@@ -2,10 +2,11 @@ $(document).ready(function(){
 
 var breakLength = 5; //number of minutes set as break
 var breakLeft = 5;	//time left to the end of break in minutes (default same as breakLength)
-var sessionLength = 1; //number of minutes set as session
-var timeLeft = sessionLength-1; //time left to the end of session  in minutes (default same as sessionLength)
+var sessionLength = 25; //number of minutes set as session
+var timeLeft = 24; //time left to the end of session  in minutes (default same as sessionLength)
 var seconds = 15;	//number of seconds left to decrase minutes left by 1
 var a = 0;
+var b = 0;
 
 
 // --------- settings for length of break and session below this line ---------
@@ -37,35 +38,24 @@ $("#session-length").html(sessionLength);
 	$("#plus-session").click(function(){
 		sessionLength++;
 		timeLeft = sessionLength;
-		timeLeft = sessionLength-1;
 		$("#session-clock").html(timeLeft+1);
 		$("#session-length").html(sessionLength);
 	});
 	
 // --------- settings for length of break and session above this line ---------
 
-
-var tt = (function checkTitleName(){
-	if(titleName = "Session"){
-		return false;
-	} else if (titleName = "Break"){
-		return true;
-	}
-})();
-
 $("#pomodoro-box").click(function clock(){
+	console.log(a);
 	if(a == 0){
 		a = 1;
 	} else {
 		a = 0;
 	} 
-
+		
 		function timer(){
-			var titleName = $("#session-text").html();
 			if( a == 1){
-				console.log("test2");
-	console.log("a2:"+titleName);
-		$("#session-clock").html((timeLeft)+":"+seconds);
+		$("#session-text").html("Session");
+		$("#session-clock").html(timeLeft+":"+seconds);
 		if(seconds<10 && seconds>0){
 			$("#session-clock").html(timeLeft+":0"+seconds);
 			seconds--;
@@ -78,19 +68,10 @@ $("#pomodoro-box").click(function clock(){
 			$("#session-clock").html(timeLeft+":"+seconds);
 			seconds--;
 		} else if(seconds==0 && timeLeft==0){
-			if(titleName == "Session"){
 			breakLeft = breakLength;
-			$("#session-text").html("Break");
-			timeLeft = breakLength-1;
-			seconds = 15;
-			$("#pomodoro-box").css('background-color','red');
-			} else if(titleName == "Break") {
-				$("#pomodoro-box").css('background-color','green');
-				$("#session-text").html("Session");
-				timeLeft = sessionLength-1;
-				seconds = 15;
-			}
-			timer();
+			clearInterval(refreshIntervalId);
+			breakTime();
+			controlNumber = 1;
 		} else{
 			seconds--;
 		}
@@ -98,21 +79,70 @@ $("#pomodoro-box").click(function clock(){
 				clearInterval(refreshIntervalId);
 				console.log("clear");
 			}
-			
 	};
-	var refreshIntervalId = setInterval(function(){
+
+		var refreshIntervalId = setInterval(function(){
 			timer();
 		}, 1000);
-		
-		
-		
 	
 	
-	
-			
 	});
 	
 	
-});
 	
+	
+	function breakTime(){
+		seconds = 15;
+		breakLeft--;
+		function timer2(){
+			if($("#session-text").text() == "Break") {
+			$("#pomodoro-box").click(function(){
+				clearInterval(refreshIntervalId2);
+				$("#session-text").html("Break");
+			});
+			}
+			if( a == 1){
+			$("#session-text").html("Break");
+				console.log("good");
+				$("#session-clock").html(breakLeft+":"+seconds);
+		if(seconds<10 && seconds>0){
+			$("#session-clock").html(breakLeft+":0"+seconds);
+			console.log("one");
+			seconds--;
+		} else if(seconds === 0 && breakLeft>0){
+			$("#session-clock").html(breakLeft+":"+"00");
+			console.log("two");
+			seconds--;
+		} else if(seconds<0 && breakLeft>0){
+			seconds = 15;
+			breakLeft--;
+			$("#session-clock").html(breakLeft+":"+seconds);
+			console.log("three");
+			seconds--;
+		} else if(seconds==0 && breakLeft==0){
+			console.log("here");
+			breakLeft = breakLength;
+			clearInterval(refreshIntervalId2);
+			clock();
 
+
+
+		} else{
+			seconds--;
+		} 
+		} else {
+				clearInterval(refreshIntervalId2);
+				console.log("clear");
+			}
+		
+		};
+				
+		
+				var refreshIntervalId2 = setInterval(function(){
+			timer2();
+		}, 1000);
+
+	};
+	
+	
+});
