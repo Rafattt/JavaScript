f45f71ee23ed4a934d2e198f89e0294c0e3028e1
Rafal Kmiecik <Rafattt@users.noreply.github.com>
2017-09-19 15:53:42 -0500

    Add files via upload

---
+++ fcc/Tic Tac Toe/script.js
@@ -0,0 +1,68 @@
+$(document).ready(function (){
+	'use strict';
+	var symbol = "";
+	var playerTwoSymbol = "";
+	var numberOfPlayers = "";
+	var controlNumber = 0;
+	var playerOneFields = [];
+	var playerTwoFields = [];
+	var winConditions = [
+		["a1","a2","a3"],
+		["a1","b1","c1"],
+		["a1","b2","c3"],
+		["b1","b2","b3"],
+		["c1","b2","a3"],
+		["a2","b2","c2"],
+		["a3","b3","c3"],
+		["c1","c2","c3"]
+	];
+	
+	$('.players').click(function(event){
+		numberOfPlayers = event.target.id;
+		console.log(numberOfPlayers);
+		if(numberOfPlayers === 'one-player'){
+		$("#number-of-players-box").css('visibility','hidden');
+		$("#choose-symbol-box").css('visibility','visible');
+		} else if(numberOfPlayers === 'two-players'){
+			$("#number-of-players-box").css('visibility','hidden');
+		$("#choose-symbol-box").css('visibility','visible');
+		}
+	});
+	
+	$('.symbols').click(function(event){
+		symbol = event.target.id;
+		if(symbol == "X"){
+					playerTwoSymbol = "O";
+					} else if(symbol == "O"){
+						playerTwoSymbol = "X";
+					}
+		$("#choose-symbol-box").css('visibility','hidden');
+		
+	});
+	
+	$('.fields').click(function(event){
+		 var field = event.target.id;
+		// $('#'+field).html(symbol);
+		 if(controlNumber == 0){
+				$('#'+field).html(symbol);
+				$('#'+field).css('pointer-events','none'); //making field inactive after click
+				playerOneFields.push(field);
+				controlNumber = 1;
+				
+				
+
+				console.log("win: "+winConditions[0]);
+				console.log("player: "+playerOneFields);
+				if($(winConditions[0]).not(playerOneFields).length === 0 && $(playerOneFields).not(winConditions[0]).length === 0){
+					document.write("You Win!");
+				}
+					
+				
+			} else if(controlNumber == 1){
+				$('#'+field).html(playerTwoSymbol);
+				$('#'+field).css('pointer-events','none');
+				controlNumber = 0;
+			}
+		 console.log(field);
+	});
+});
