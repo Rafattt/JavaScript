2b8a74a7dc3b804a98d99188ed257d544c798b74
Rafal Kmiecik <Rafattt@users.noreply.github.com>
2017-09-21 15:34:39 -0500

    Add files via upload

---
+++ fcc/Tic Tac Toe/scriptFirstVersion.js
@@ -0,0 +1,108 @@
+$(document).ready(function (){
+	'use strict';
+	var symbol = "";
+	var playerTwoSymbol = "";
+	var numberOfPlayers = "";
+	var controlNumber = 0;
+	var playerOneFields = [];
+	var playerTwoFields = [];
+	var winConditions = [
+		["a1","a2","a3"],
+		["a1","b1","c1"],
+		["a1","b2","c3"],
+		["b1","b2","b3"],
+		["c1","b2","a3"],
+		["a2","b2","c2"],
+		["a3","b3","c3"],
+		["c1","c2","c3"]
+	];
+	
+	$('.players').click(function(event){
+		numberOfPlayers = event.target.id;
+		console.log(numberOfPlayers);
+		if(numberOfPlayers === 'one-player'){
+		$("#number-of-players-box").css('visibility','hidden');
+		$("#choose-symbol-box").css('visibility','visible');
+		} else if(numberOfPlayers === 'two-players'){
+			$("#number-of-players-box").css('visibility','hidden');
+		$("#choose-symbol-box").css('visibility','visible');
+		
+		$('.fields').click(function(event){
+		 var field = event.target.id;
+		// $('#'+field).html(symbol);
+		 if(controlNumber == 0){
+				$('#'+field).html(symbol);
+				$('#'+field).css('pointer-events','none'); //making field inactive after click
+				playerOneFields.push(field);
+				controlNumber = 1;
+				
+				for(var i= 0; i<8;i++){
+					// fragment copied from stack starts here
+				var res = winConditions[i].filter(function(v) { // iterate over the array
+  // check element present in the second array
+  return playerOneFields.indexOf(v) > -1;
+  // or array2.includes(v)
+})
+
+// fragment copied from stack ends here
+if(res.length === 3){
+	document.write("Player One Win");
+}
+
+
+				}
+
+
+				/*if($(winConditions[0]).not(playerOneFields).length === 0 && $(playerOneFields).not(winConditions[0]).length === 0){
+					document.write("You Win!");
+				}*/
+					
+				
+			} else if(controlNumber == 1){
+				$('#'+field).html(playerTwoSymbol);
+				$('#'+field).css('pointer-events','none');
+				playerTwoFields.push(field);
+				controlNumber = 0;
+				
+				for(var i= 0; i<8;i++){
+					
+					// fragment copied from stack starts here
+				var res2 = winConditions[i].filter(function(v) { // iterate over the array
+				console.log(playerTwoFields);
+  // check element present in the second array
+  return playerTwoFields.indexOf(v) > -1;
+  // or array2.includes(v)
+  
+})
+
+
+console.log("res2: "+res2);
+// fragment copied from stack ends here
+if(res2.length === 3){
+	document.write("Player Two Win");
+}
+
+
+				}
+
+				
+			}
+		 console.log(field);
+	});
+		
+		}
+	});
+	
+	$('.symbols').click(function(event){
+		symbol = event.target.id;
+		if(symbol == "X"){
+					playerTwoSymbol = "O";
+					} else if(symbol == "O"){
+						playerTwoSymbol = "X";
+					}
+		$("#choose-symbol-box").css('visibility','hidden');
+		
+	});
+	
+	
+});
