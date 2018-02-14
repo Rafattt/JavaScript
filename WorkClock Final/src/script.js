5a20e47f7bc47c76cbaed77a7c503ca45fd804ff
Rafal Kmiecik <Rafattt@users.noreply.github.com>
2018-02-14 12:16:57 -0600

    Add files via upload

--- WorkClock Final/src/script.js
+++ WorkClock Final/src/script.js
@@ -1,158 +1,97 @@
-let startTime2 = "";
-let startTimeInput = "";
-let timeElapsed = "";
-let expectedEndTime = "";
-let expectedEndTimeTest = ":00";
-let hourRate = "19";
-let tax = "";
-let currentDate = "";
-let currentTime = "";
-let controlNumber = 0;
-$( document ).ready(function() {
+let startTimeInput = ""; // var to store value from input field. hour and minutes of start
+let startTime = ""; //store time of start after conversion to actual time units
+let timeElapsed = ""; //time elapsed from start to current time
+let timeLeft = ""; //time left to end
+let expectedEndTimeInput = ""; // var to store value from input field. hour and minutes of expected end
+let hourRateInput = ""; // var to store value from input field. rate hours in dollars
+let taxInput = "" // var to store value from input field. tax in percent
+let currentDate = ""; // var to store current date from Date.now()
+let currentTime = ""; // currentDate converted to hh:mm:ss format
+let validArray = [0,0,0];
+let controlNumber = 0; // 0 if data wasn't entered 1 if data was entered, determines if starts "timeRemaining" and "timeElapsed" function
+let progressBarWidthZero = "";
+const midnight = 86400000; //midnight (24:00) in miliseconds, needed for calcuating hours between two days
     
-    $('#ok-button').click(function(){
-        controlNumber = 1;
+$( document ).ready(function() {
+
+    $('#ok-button').click(function(){  //clicking "ok" button in input box starting this:
         getUserData();
         if(checkIfBlank() === true){
-            $('#input-box').css("display","none");
+           
            startTime = $('#start').val();
            $('#time-start-numbers').html(startTimeInput+':00');
            
         }
+        incorrectInfo('#start-time-incorrect', startTimeInput, validArray, 0);
+        incorrectInfo('#end-time-incorrect', expectedEndTimeInput, validArray, 1);
+        incorrectNumeric('#rate-incorrect', hourRateInput, validArray, 2);
+        controlNumber = 1;
+        if(($('#tax').val())!==""){
+            incorrectNumeric('#tax-incorrect', taxInput, validArray, 3);
+        } else { //if tax field is empty taxInput is 0
+            taxInput = 0;
+        }
+        //incorrectNumeric('#tax-incorrect', taxInput, validArray, 3);
+        if(validArray[0] === 1 && validArray[1] === 1 && validArray[2] === 1){ //if format is correct in 3 fields input window will disapear after clicking "ok" button
+            $('#input-box').css("display","none");
+        }
     })
 
-    function getUserData(){
-        if(controlNumber === 1){
-            startTimeInput = $('#start').val();
-            expectedEndTime = $('#end').val();
-            hourRate = $('#rate').val();
-            if(($('#tax').val())!==""){
-                tax = $('#tax').val();
-            }
-            
-        }
-        
-    }
-
-    $('#start, #end, #rate').blur(function(){
-        if(checkIfBlank() === true){
-            $('#ok-button').removeClass("disabled");
-            $('#ok-button').addClass("active");
-        }
-    });
-
-    function checkIfBlank(){
+    function checkIfBlank(){ //checking if input fields are blank
         if($('#start').val() !== "" && $('#end').val() !== ""
          && $('#rate').val() !== ""){
              return true;
          }
     }
 
-    function setTimeFormat(timeUnit){
-        if(timeUnit<10){
-            let zero = '0';
-            timeUnit = zero.concat(timeUnit);
-        }
-        return timeUnit;
+   (function enableDisbaleOk(){ //checking if ok button should be active
+    if(checkIfBlank() === true){
+        $('#ok-button').removeClass("disabled");
+        $('#ok-button').addClass("active");
+    }
+    let t = setTimeout(enableDisbaleOk, 500);
+   })();
+        
+    function getUserData(){ //getting data from input fields and set it as variables
+     
+            startTimeInput = $('#start').val();
+            expectedEndTimeInput = $('#end').val();
+            hourRateInput = $('#rate').val();
+            if(($('#tax').val())!==""){
+                taxInput = $('#tax').val();
+            }
     }
 
-    (function showCurrentTime(){
-        currentDate = new Date($.now());
-            currentTime = setTimeFormat(currentDate.getHours()) + ':' + setTimeFormat(currentDate.getMinutes()) + ':' +
-            setTimeFormat(currentDate.getSeconds());
-                $('#current-time-numbers').html(currentTime);
-            
-       
-        var t = setTimeout(showCurrentTime, 500);
 
-    })();
-
-    (function timeElapsedFunction(){
-        if(controlNumber === 1){
-            startTime = startTimeInput.split(":");
-            let currentTimeSplitted = currentTime.split(":");
-            let currentTimeMili = (currentTimeSplitted[0] * (60000 * 60))+(currentTimeSplitted[1] * 60000) + (currentTimeSplitted[2] * 1000);
-            let startTime2 =  (startTime[0] * (60000 * 60))+(startTime[1] * 60000);
-           
-    
-            timeElapsed = currentTimeMili - startTime2;
-            let tempTime = timeElapsed;
-            
-            
-          
-            $('#time-elapsed-numbers').html( msToTime(tempTime));
+    function checkIfValidFormat(time){ //check if entered time format is valid
+        let valid = /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/;
+        if(valid.test(time) === true){
+           return true;
+        }else{
+            return false;
         }
-       
-        var t = setTimeout(timeElapsedFunction, 500);
-    })();
-
-    (function timeRemaining(){
-        if(controlNumber === 1){
-            let expectedEndTimeSplitted = expectedEndTime.split(":");
-            let expectedEndTimeMili = (expectedEndTimeSplitted[0] * (60000 * 60))+(expectedEndTimeSplitted[1] * 60000);
-            let currentTimeSplitted = currentTime.split(":");
-            let currentTimeMili = (currentTimeSplitted[0] * (60000 * 60))+(currentTimeSplitted[1] * 60000) + (currentTimeSplitted[2] * 1000);
-            let timeLeft = expectedEndTimeMili - currentTimeMili;
-            $('#time-left-numbers').html( msToTime(timeLeft));
-    //progress bar starts here
-    let startTime2 =  (startTime[0] * (60000 * 60))+(startTime[1] * 60000);
-    let progressBarWidthZero = expectedEndTimeMili - startTime2;
-    let progressBarWidthHundred = 0;
-    let progressBarWidthCurrent = "";
-    // count percent :timeElapsed/(progressBarWidthZero/100)
-    
-    let currentPercent = parseInt(timeElapsed/(progressBarWidthZero/100));
-    $('#progress').css('width', currentPercent+'%');
-    console.log('expectedEndTimeMili '+expectedEndTimeMili);
-    console.log('percent '+currentPercent);
-    console.log('startTime2 '+ startTime2);
-    console.log(progressBarWidthZero);
-    //progress bar ends here
-        }
-      
-
-        var t = setTimeout(timeRemaining, 500);
-    })();
-
-    function msToTime(duration) {
-        var milliseconds = parseInt((duration%1000)/100)
-            , seconds = parseInt((duration/1000)%60)
-            , minutes = parseInt((duration/(1000*60))%60)
-            , hours = parseInt((duration/(1000*60*60))%24);
-    
-        hours = (hours < 10) ? "0" + hours : hours;
-        minutes = (minutes < 10) ? "0" + minutes : minutes;
-        seconds = (seconds < 10) ? "0" + seconds : seconds;
-    
-        return hours + ":" + minutes + ":" + seconds;
     }
 
-    (function earnings(){
-        
-        let earned = (Math.round(((hourRate/3600)*(timeElapsed/1000)) * 100) / 100).toFixed(2);
-        if(($("#lunch-yes").prop("checked"))=== true){
-            earned = earned - hourRate/2;
-            earned = earned.toFixed(2);
-        } 
-        let taxDeduct = (earned/100)*tax;
-        taxDeduct = taxDeduct.toFixed(2);
-        let realEarned = earned-taxDeduct;
-        realEarned = realEarned.toFixed(2);
-       
-        $('#money-earned-numbers').html(realEarned+'$');
-        if(tax!==""){
-            $('#tax-numbers').html(taxDeduct+'$');
-            $('#gross-numbers').html(earned+'$');
+    function incorrectInfo(id, input, valid, index){ //checking if entered info is in correct format and show information if not (format hh:mm)
+        if(checkIfValidFormat(input)=== false){
+            $(id).removeClass('inncorrect').addClass('incorrect-active');
+            valid[index]=0;
+        } else {
+            $(id).removeClass('incorrect-active').addClass('inncorrect');
+            valid[index]=1;
+            return true;
         }
-       
-        var t = setTimeout(earnings, 500);
-        
-    })();
+    }
 
-
-    
+    function incorrectNumeric(id, input, valid, index){ //checking if entered info is in correct format and show information if not (numbers)
+        if( $.isNumeric(input)=== false){
+            $(id).removeClass('inncorrect').addClass('incorrect-active');
+            valid[index]=0;
+        } else {
+            $(id).removeClass('incorrect-active').addClass('inncorrect');
+            valid[index]=1;
+            return true;
+        }
+    }
 
 });
-
-
-// USE THIS LATER - ALWAYS SHOW TWO DECIMAL PLACEES -    parseFloat(Math.round(num3 * 100) / 100).toFixed(2);
